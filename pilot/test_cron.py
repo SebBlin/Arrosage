e76c56bbbcb8b4548acb7d7b56f4d860d74b0d13
SebBlin <sebastien.blin@gmail.com>
2020-05-24 13:52:51 +0000

    test cron

--- pilot/test_cron.py
+++ pilot/test_cron.py
@@ -1,4 +1,18 @@
 from crontab import CronTab
-cron = CronTab(user='pi')
 
-print (cron)
+my_cron = CronTab(user='pi')
+found_job = False
+
+for job in my_cron:
+    print('job', job)
+    if job.comment == 'test':
+        found_job = True
+        job.minute.every(5)
+
+if not(found_job) :
+    job = my_cron.new(command='python3 /home/pi/Arrosage/pilot/writeDate.py', comment='test')
+    job.minute.every(1)
+
+my_cron.write()
+
+print (my_cron)
--- pilot/webapp/src/index.js
+++ pilot/webapp/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function formatName(user) {
+  return user.firstName + ' : ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
